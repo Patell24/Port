c771f62d2585495dc53f4722a061a9e852126e35
Patell24 <realmeuhd4ktv@gmail.com>
2024-12-21 08:32:15 +0000

    particles

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
+import Particle from "./components/Particle";
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
+        <Particle />
         <Hero />
         <Features />
       </main>
